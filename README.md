d78e3ad77128d56b5255e2f42dac41b430dcbc28
Matthew Weier O'Phinney <matthew@zend.com>
2013-07-02 16:30:08 -0500

    Initial import
    - All scripts, configuration templates, and license.

---
+++ README.md
@@ -0,0 +1,29 @@
+changelog\_generator.php
+========================
+
+This project provides a simple way to create an ordered list of issues and pull
+requests closed with a given milestone on GitHub. It uses Zend Framework's
+`Zend\Http` component to communicate with the GitHub API.
+
+Installation
+------------
+
+Use [Composer](https://getcomposer.org) to install dependencies:
+
+```sh
+php /path/to/composer.phar install
+```
+
+Usage
+-----
+
+Copy `config/config.php.dist` to `config/config.php` and edit the new file to
+add your GitHub API token, the user or organization, the repository, and the
+milestone identifier for which you want to generate a changelog.
+
+Once you have, simply invoke the `changelog_generator.php` script using your
+PHP interpreter, and pipe the output to a file:
+
+```sh
+php changelog_generator.php > changelog.md
+```
---
+++ changelog_generator.php
@@ -0,0 +1,72 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+ini_set('display_errors', true);
+error_reporting(E_ALL | E_STRICT);
+
+$config = include __DIR__ . '/config/config.php';
+if (false === $config) {
+    echo "Failed to load config; please create a config/config.php file based on config/config.php.dist.";
+    exit(1);
+}
+
+$token     = $config['token'];       // Github API token
+$user      = $config['user'];        // Your user or organization
+$repo      = $config['repo'];        // The repository you're getting the changelog for
+$milestone = $config['milestone'];   // The milestone ID
+
+$client = new Zend\Http\Client();
+$client->setOptions(array(
+    'adapter' => 'Zend\Http\Client\Adapter\Curl',
+));
+
+$request = $client->getRequest();
+$headers = $request->getHeaders();
+
+$headers->addHeaderLine("Authorization", "token $token");
+
+$client->setUri("https://api.github.com/repos/$user/$repo/issues?milestone=$milestone&state=closed&per_page=100");
+$client->setMethod('GET');
+$issues = array();
+$done   = false;
+$error  = false;
+$i      = 0;
+do {
+    $response = $client->send();
+    $json     = $response->getBody();
+    $payload  = json_decode($json);
+    if (!is_array($payload)) {
+        $error = $payload;
+    }
+    if (is_array($payload)) {
+        $issues = array_merge($issues, $payload);
+        $linkHeader = $response->getHeaders()->get('Link');
+        if (!$linkHeader) {
+            $done = true;
+        }
+        if ($linkHeader) {
+            $links = $linkHeader->getFieldValue();
+            $links = explode(', ', $links);
+            foreach ($links as $link) {
+                $matches = array();
+                if (preg_match('#<(?P<url>.*)>; rel="next"#', $link, $matches)) {
+                    $client->setUri($matches['url']);
+                }
+            }
+        }
+    }
+    $i += 1;
+} while (!$done && !$error && ($i < 5));
+
+if ($error) {
+    var_export($error);
+    exit(0);
+}
+
+echo "Total issues resolved: **" . count($issues) . "**\n";
+
+foreach ($issues as $index => $issue) {
+    $issues[$issue->number] = sprintf('- [%d: %s](%s)', $issue->number, $issue->title, $issue->html_url);
+    unset($issues[$index]);
+}
+ksort($issues);
+echo implode("\n", $issues) . "";
